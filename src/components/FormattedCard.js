45da1958d74fd8aecb478f84c066043686ac0bdc
aryanindarapu <aryan1pro@gmail.com>
2021-01-02 08:10:08 -0500

    removed ph no

--- src/components/FormattedCard.js
+++ src/components/FormattedCard.js
@@ -5,10 +5,10 @@ import '../index.css';
 export const FormattedCard = ({ place }) => {
   return (
     <Card>
-      <Card.Body>
-        <Card.Title>{place.name}</Card.Title>
-        <Card.Subtitle className="mb-2 text-muted">{place.vicinity}</Card.Subtitle>
-        <Card.Link href="tel:3177025698">{place.formatted_phone_number}</Card.Link>
+      <Card.Body className="cardbody" onClick={() => {console.log("clicked")}}>
+        <Card.Title style={{fontWeight: 'bold'}}>{place.name}</Card.Title>
+        <Card.Subtitle className="mb-2 text-muted" style={{textTransform: 'uppercase'}}>{place.vicinity}</Card.Subtitle>
+        <Card.Link href="tel:">{place.formatted_phone_number}</Card.Link>
         <Card.Link href="#">Another Link</Card.Link>
       </Card.Body>
     </Card>
